7a36b433d39a012750fd5644e661747198714a18
jisung594 <jisung594@gmail.com>
2019-06-07 20:21:15 -0400

    practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import IncidentList from './Containers/IncidentList'
+import './App.css'
 
 import { connect } from 'react-redux'
 import { getIncidents } from './Redux/actions.js'
@@ -12,6 +13,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="navbar">
+          <h2>OPTION1</h2>
+          <h2>OPTION2</h2>
+          <h2>OPTION3</h2>
+          {/* navbar menu option icon */}
+          {/* navbar menu option icon */}
+          {/* navbar menu option icon */}
+        </div>
         <IncidentList incidents={this.props.incidents}/>
       </div>
     );
