cbb53538290c5df34048b1598e1799ea77232a71
jisung594 <jisung594@gmail.com>
2019-06-08 22:40:56 -0400

    started incorporating custom routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component,Fragment} from 'react';
 import IncidentList from './Containers/IncidentList'
-import './App.css'
+import Homepage from './Components/Homepage'
+import './stylesheets/App.css'
 
 import { connect } from 'react-redux'
 import { getIncidents } from './Redux/actions.js'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
@@ -12,17 +14,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="navbar">
-          <h2>OPTION1</h2>
-          <h2>OPTION2</h2>
-          <h2>OPTION3</h2>
-          {/* navbar menu option icon */}
-          {/* navbar menu option icon */}
-          {/* navbar menu option icon */}
-        </div>
-        <IncidentList incidents={this.props.incidents}/>
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <div className="App">
+            <div className="navbar">
+              <Link to='/' className='navbar-options'>HOME</Link>
+              <Link to='/report' className='navbar-options'>REPORT</Link>
+              <Link to='/listed' className='navbar-options'>LISTED</Link>
+            </div>
+
+            <div className="main-content">
+              <Switch>
+                <Route path='/' component={Homepage} exact/>
+                {/* CREATE A COMPONENT FOR REPORT FORM, AS WELL AS A ROUTE HERE */}
+                <Route path='/listed' render={()=>{
+                  return <IncidentList incidents={this.props.incidents}/>
+                }} />
+              </Switch>
+            </div>
+
+          </div>
+        </Fragment>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../stylesheets/Homepage.css'
+
+const Homepage = (props) => {
+  // let { } => props;
+
+  return (
+    <div className="homepage">
+      <h1>Dude, where's my bike?</h1>
+    </div>
+  )
+}
+
+export default Homepage
